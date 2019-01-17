bb24616d1b016316c05c9815f33b9e141c9cd81a
featherbear <andrew.j.wong@outlook.com>
2019-01-17 11:08:21 +1100

    site | Open description links in a new window/tab

--- site/assets/js/game.js
+++ site/assets/js/game.js
@@ -17,7 +17,13 @@ function openModalQuestion(questionData, srcElem) {
     modal.querySelector("[name=flag]").placeholder = "FLAG{...}";
     modal.querySelector('[name=title]').innerText = questionData.title;
     modal.querySelector('[name=category]').innerText = categories[questionData.category] || "";
-    modal.querySelector('[name=description]').innerHTML = questionData.description
+    modal.querySelector('[name=description]').innerHTML = questionData.description;
+    
+    // Open links in a new window/tab
+    for (elem of modal.querySelector('[name=description]').getElementsByTagName('a')) {
+        if (!elem.target) elem.target = "_blank";
+    }
+    
     // modal.querySelector('[name=description]').srcdoc = "<link rel='stylesheet' href='/assets/css/iframe.sandbox.css'/>" + questionData.description; // XSS AWAY
 
     const flagSubmissionDisable = function () {
