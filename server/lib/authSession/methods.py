bbaea378bdfde18b6f33ae57a635ec56819c5935
Andrew Wong <andrew.j.wong@outlook.com>
2019-02-05 11:10:11 +1100

    server | Update auth session time from 30 minutes to 2 hours

--- server/lib/authSession/methods.py
+++ server/lib/authSession/methods.py
@@ -7,12 +7,12 @@ from .SQLMethod import SQLMethod
 def createSession(user: int):
     currentTime = int(time())
     token = sha1((str(user) + ":" + str(currentTime)).encode()).hexdigest()
-    SQLMethod.newSession(user, currentTime + 30 * 60, token)
+    SQLMethod.newSession(user, currentTime + 120 * 60, token)
     return token
 
 
 def updateSession(token: str):
-    return not not SQLMethod.updateSession(token, int(time()) + 30 * 60)
+    return not not SQLMethod.updateSession(token, int(time()) + 120 * 60)
 
 
 def getSession(token: str):
