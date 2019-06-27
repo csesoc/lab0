e08c1d1cbc5b49e6be73c0a36d66877d383b7bfa
Andrew Wong <andrew.j.wong@outlook.com>
2019-06-28 00:31:52 +1000

    Add example settings file. Auto settings file creation on first startup

--- server/lib/config.py
+++ server/lib/config.py
@@ -2,9 +2,26 @@ config: dict
 
 
 def readConfig(file="settings.ini") -> dict:
+    _defaultFile = "settings.ini"
+    _skeletonFile = "settings.example.ini"
+    
+    import os.path
+    if file == _defaultFile and not os.path.isfile(file):
+      if not os.path.isfile(_skeletonFile):
+        raise Exception("Missing settings skeleton file!")
+      import shutil
+      shutil.copy(_skeletonFile, _defaultFile)
+      print("Created settings.ini file.\nPlease modify your settings then run the server again")
+      import sys
+      sys.exit(1)
+
     import configparser
     _config = configparser.ConfigParser()
-    _config.read(file)
+    try:
+      if len(_config.read(file)) == 0:
+        raise Exception()
+    except Exception:
+      raise Exception(f"Could not load settings from: {file}")
     global config
     config = _config._sections
     return config
