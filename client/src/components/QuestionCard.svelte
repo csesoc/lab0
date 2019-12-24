b6ae943032f5cf38dab3aa5ffa6e93b2faf195be
Andrew Wong <andrew.j.wong@outlook.com>
2019-12-25 00:38:30 +1100

    Port Question Tile

---
+++ client/src/components/QuestionCard.svelte
@@ -0,0 +1,22 @@
+
+<script>
+export let title;
+export let category;
+export let points;
+export let active;
+
+let card;
+
+function openModal() {
+    // Stub
+}
+</script>
+<style>
+
+</style>
+
+<article class:active class="tile is-child notification is-3" bind:this={card} on:click={openModal}>
+  <h1 class="title" >{title}</h1>
+  <h2>{category}</h2>
+  <h3>{points}</h3>
+</article>
